f10d4998cdb64b87937f8aea075a38ee762d7196
Nabillera <vatcharadzenini@gmail.com>
2025-03-13 21:01:46 +0400

    add function to fetch status, priority, department

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 
 function App() {
-  return <div>
-    <Navbar/>
-  </div>;
+  const categories = ["statuses", "priorities", "departments"];
+  const [fetchedData, setFetchedData] = useState({
+    statuses: [],
+    priorities: [],
+    departments: []
+  });
+
+  const fetchData = () => {
+    categories.map((category) => {
+      return fetch(`https://momentum.redberryinternship.ge/api/${category}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setFetchedData((prev) => {
+            return {
+              ...prev,
+              [category]: data,
+            };
+          });
+        });
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
