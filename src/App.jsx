8e18e6ee1a6b3eec168ef22f2a7ef85c87add9df
Nabillera <vatcharadzenini@gmail.com>
2025-03-13 22:54:13 +0400

    modify function to fetch employees and tasks

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,33 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 
+const TOKEN = "9e6c009b-1288-4a6b-a018-6a9e2b68c3d9";
+
 function App() {
-  const categories = ["statuses", "priorities", "departments"];
+  const categories = [
+    "statuses",
+    "priorities",
+    "departments",
+    "employees",
+    "tasks",
+  ];
+
   const [fetchedData, setFetchedData] = useState({
     statuses: [],
     priorities: [],
-    departments: []
+    departments: [],
+    employees: [],
+    tasks: [],
   });
 
   const fetchData = () => {
     categories.map((category) => {
-      return fetch(`https://momentum.redberryinternship.ge/api/${category}`)
+      return fetch(`https://momentum.redberryinternship.ge/api/${category}`, {
+        headers: {
+          "Authorization": `Bearer ${TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      })
         .then((response) => response.json())
         .then((data) => {
           setFetchedData((prev) => {
