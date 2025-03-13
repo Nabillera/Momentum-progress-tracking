6f6084795f55df3e981b64fb47710ad71c8f6b3c
Nabillera <vatcharadzenini@gmail.com>
2025-03-14 01:49:11 +0400

    add navbar button hover effect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,17 +4,17 @@ import PlusSign from "../assets/Plus.svg";
 
 export default function Navbar() {
   return (
-    <div className="fixed flex w-[100%] items-center justify-between px-[120px] py-[30px]">
+    <div className="fixed flex w-[100%] items-center justify-between bg-white px-[120px] py-[30px]">
       <div className="flex items-center gap-x-[8px]">
         <img src={Momentum} />
         <img src={Hourglass} />
       </div>
 
       <div className="flex gap-x-[40px]">
-        <button className="border-blue-violet cursor-pointer rounded-[5px] border-[1px] px-[20px] py-[9px] leading-[16px]">
+        <button className="cursor-pointer rounded-[5px] border-[1px] border-[#8338EC] px-[20px] py-[9px] leading-[16px] hover:border-[#B588F4] transition">
           თანამშრომლის შექმნა
         </button>
-        <button className="bg-blue-violet border-blue-violet cursor-pointer rounded-[5px] border-[1px] px-[20px] py-[9px] leading-[16px] text-white">
+        <button className="border-blue-violet cursor-pointer rounded-[5px] border-[1px] bg-[#8338EC] px-[20px] py-[9px] leading-[16px] text-white hover:bg-[#B588F4] transition">
           <div className="flex items-center gap-x-[4px]">
             <img src={PlusSign} /> შექმენი ახალი დავალება
           </div>
