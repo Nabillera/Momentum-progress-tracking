8902d4231fac2b4cb3a2290277f7db05c48ddb55
Nabillera <vatcharadzenini@gmail.com>
2025-03-19 23:04:22 +0400

    add task details page

---
+++ src/components/OpenTaskDetails.jsx
@@ -0,0 +1,66 @@
+import PieChart from "../assets/PieChart.svg";
+import Employee from "../assets/Employee.svg";
+import Calendar from "../assets/Calendar.svg";
+import Dropdown from "../components/Dropdown";
+import { format } from "date-fns";
+import { ka } from "date-fns/locale";
+
+export default function OpenTaskDetails({ taskId, details, statuses }) {
+  const dueDate = details.due_date.slice(0, 10);
+  const formattedDate = format(dueDate, "E - d/M/yyyy", { locale: ka });
+
+  const handleChangeStatus = (name, statusId) => {
+    // put request
+  };
+
+  return (
+    <div>
+      <h2 className="py-[10px] text-[24px] font-medium text-[#2A2A2A]">
+        დავალების დეტალები
+      </h2>
+      <div className="flex">
+        <div className="flex w-[715px] flex-col">
+          <div className="flex h-[70px] items-center gap-[70px]">
+            <div className="flex w-[164px] gap-[6px]">
+              <img src={PieChart} />
+              <span className="text-[#474747]">სტატუსი</span>
+            </div>
+            <div className="w-[259px]">
+              <Dropdown
+                options={statuses}
+                defaultValue={details.status.name}
+                onFillField={handleChangeStatus}
+              />
+            </div>
+          </div>
+
+          <div className="flex h-[70px] items-center gap-[70px]">
+            <div className="flex w-[164px] gap-[6px]">
+              <img src={Employee} />
+              <span className="text-[#474747]">თანამშრომელი</span>
+            </div>
+            <div className="flex items-center gap-[12px]">
+              <img src={details.employee.avatar} className="w-[32px]" />
+              <div className="flex flex-col">
+                <span className="text-[11px] text-[#474747]">
+                  {details.employee.department.name}
+                </span>
+                <span className="text-[14px] text-[#0D0F10]">
+                  {details.employee.name + " " + details.employee.surname}
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex h-[70px] items-center gap-[70px]">
+            <div className="flex w-[164px] gap-[6px]">
+              <img src={Calendar} />
+              <span className="text-[#474747]">დავალების ვადა</span>
+            </div>
+            <span className="text-[14px] text-[#0D0F10]">{formattedDate}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/OpenTaskPage.jsx
@@ -0,0 +1,34 @@
+import { useParams } from "react-router-dom";
+import High from "../assets/PriorityHigh.svg";
+import Medium from "../assets/PriorityMedium.svg";
+import Low from "../assets/PriorityLow.svg";
+import DepartmentIcon from "../components/DepartmentIcon";
+import OpenTaskDetails from "../components/OpenTaskDetails";
+
+export default function OpenTaskPage({ data, priorities, statuses }) {
+  const taskId = useParams().taskId;
+  const taskData = data.find((task) => task.id == taskId);
+  const priorityIcon =
+    priorities.id == 1 ? Low : priorities.id == 2 ? Medium : High;
+
+  return (
+    <div className="flex gap-[223px] pt-[140px] pl-[121px]">
+      <div className="flex flex-col">
+        <div className="flex gap-[18px]">
+          <img src={priorityIcon} className="w-[106px]" />
+          <DepartmentIcon departmentId={taskData.employee.department.id} />
+        </div>
+        <h1 className="pt-[12px] pb-[26px] text-[34px] font-semibold text-[#212529]">
+          {taskData.name}
+        </h1>
+        <p className="text-[18px]">{taskData.description}</p>
+        <OpenTaskDetails
+          taskId={taskId}
+          details={taskData}
+          statuses={statuses}
+        />
+      </div>
+      {/* <CommentSection /> */}
+    </div>
+  );
+}
