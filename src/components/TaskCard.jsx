397c5bd62736dd570f9b62e0194f3ebfcfef8837
Nabillera <vatcharadzenini@gmail.com>
2025-03-19 17:15:03 +0400

    task card design touch-ups

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -2,12 +2,14 @@ import High from "../assets/PriorityHigh.svg";
 import Medium from "../assets/PriorityMedium.svg";
 import Low from "../assets/PriorityLow.svg";
 import Comments from "../assets/Comments.svg";
-import { format } from "date-fns";
 import DepartmentIcon from "./DepartmentIcon";
+import { format } from "date-fns";
+import { ka } from "date-fns/locale";
 
 export default function TaskCard({ taskData, color }) {
+  console.log(taskData);
   const dueDate = taskData.due_date.slice(0, 10);
-  const formattedDate = format(dueDate, "dd MMM, yyyy");
+  const formattedDate = format(dueDate, "dd MMM, yyyy", { locale: ka });
   const priorityIcon =
     taskData.priority.id == 1 ? Low : taskData.priority.id == 2 ? Medium : High;
 
@@ -21,11 +23,15 @@ export default function TaskCard({ taskData, color }) {
           <img src={priorityIcon} />
           <DepartmentIcon departmentId={taskData.department.id} isTaskCard />
         </div>
-        <span className="text-[12px]">{formattedDate}</span>
+        <span className="text-[12px] text-[#212529]">{formattedDate}</span>
       </div>
       <div className="flex flex-col">
-        <span className="text-[15px] font-medium">{taskData.name}</span>
-        <span className="text-[14px]">{taskData.description}</span>
+        <span className="text-[15px] font-medium text-[#212529]">
+          {taskData.name}
+        </span>
+        <span className="text-[14px] text-[#343A40]">
+          {taskData.description}
+        </span>
       </div>
       <div className="flex justify-between">
         <img
@@ -34,7 +40,7 @@ export default function TaskCard({ taskData, color }) {
         />
         <div className="flex items-center gap-x-[4px]">
           <img src={Comments} />
-          <span className="text-[14px]">count</span>
+          <span className="text-[14px] text-[#212529]">{taskData.total_comments}</span>
         </div>
       </div>
     </div>
