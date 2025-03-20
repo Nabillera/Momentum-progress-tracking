e2982a4a0e23f3387d042c20c2cd6494b26113f5
Nabillera <vatcharadzenini@gmail.com>
2025-03-21 01:01:17 +0400

    manage checked options in filter list

--- src/components/FilterItem.jsx
+++ src/components/FilterItem.jsx
@@ -3,14 +3,37 @@ import Tick from "../assets/Tick.svg";
 import CheckPurple from "../assets/CheckPurple.svg";
 import TickPurple from "../assets/TickPurple.svg";
 
-export default function FilterItem({ category, option }) {
-  const checkIcon = category == "დეპარტამენტი" ? Check : CheckPurple;
-  const tickIcon = category == "დეპარტამენტი" ? Tick : TickPurple;
+export default function FilterItem({
+  onSelect,
+  category,
+  option,
+  isChecked,
+  icon,
+}) {
+  const checkIcon = category == "department" ? Check : CheckPurple;
+  const tickIcon = category == "department" ? Tick : TickPurple;
+
+  const handleCheckbox = () => {
+    onSelect(option);
+  };
 
   return (
     <li className="relative flex items-center gap-x-[15px]">
-      <img src={checkIcon} />
-      <span className="leading-[22px]">{option}</span>
+      {isChecked && (
+        <img
+          src={tickIcon}
+          onClick={() => handleCheckbox()}
+          className="absolute left-[4px]"
+        />
+      )}
+      <img src={checkIcon} onClick={() => handleCheckbox()} />
+      <div
+        className="flex items-center gap-[10px]"
+        onClick={() => handleCheckbox()}
+      >
+        {icon && <img src={icon} className="w-[28px]" />}
+        <span className="leading-[22px]">{option}</span>
+      </div>
     </li>
   );
 }
--- src/components/FilterList.jsx
+++ src/components/FilterList.jsx
@@ -1,20 +1,62 @@
+import { useState } from "react";
 import FilterItem from "./FilterItem";
 
 export default function FilterList({ category, list }) {
+  const [checkedFilters, setCheckedFilters] = useState([]);
+
+  const handleFilterCheck = (optionValue) => {
+    if (category == "employee") {
+      setCheckedFilters((prev) => {
+        if (prev.includes(optionValue)) {
+          return [];
+        } else {
+          return [optionValue];
+        }
+      });
+    } else {
+      setCheckedFilters((prev) => {
+        if (prev.includes(optionValue)) {
+          const copy = [...prev];
+          const index = copy.indexOf(optionValue);
+          copy.splice(index, 1);
+          return copy;
+        } else {
+          return [...prev, optionValue];
+        }
+      });
+    }
+  };
+
+  const handleSubmitFilters = () => {
+    // choose button function
+  };
+
   return (
     <div className="absolute z-0 mt-[11px] flex h-[274px] w-[688px] flex-col gap-y-[24px] rounded-[10px] border-[1px] border-[#8338EC] bg-white px-[30px] pt-[40px] pb-[20px]">
       <ul className="flex h-[156px] flex-col gap-y-[22px] overflow-y-scroll">
         {list.map((item, i) => {
+          const icon = item.avatar ?? undefined;
           const filterOption =
             item.surname == undefined
               ? item.name
               : item.name + " " + item.surname;
           return (
-            <FilterItem category={category} option={filterOption} key={i} />
+            <FilterItem
+              onSelect={handleFilterCheck}
+              category={category}
+              option={filterOption}
+              icon={icon}
+              optionId={item.id}
+              isChecked={checkedFilters.includes(filterOption)}
+              key={i}
+            />
           );
         })}
       </ul>
-      <button className="h-[35px] w-[155px] cursor-pointer self-end rounded-[20px] bg-[#8338EC] text-white transition hover:bg-[#B588F4]">
+      <button
+        className="h-[35px] w-[155px] cursor-pointer self-end rounded-[20px] bg-[#8338EC] text-white transition hover:bg-[#B588F4]"
+        onClick={handleSubmitFilters}
+      >
         არჩევა
       </button>
     </div>
